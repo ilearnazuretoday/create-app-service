aecc6541b4993d6c6dd221a5b3911bf13d2e9059
decoder <decoder@lilve.de>
2021-07-22 15:03:53 +0200

    output

--- terraform/output.tf
+++ terraform/output.tf
@@ -6,6 +6,10 @@ output "app_service_default_hostname" {
   value = "https://${azurerm_app_service.main.default_site_hostname}"
 }
 
+output "acr_name" {
+  value = azurerm_container_registry.acr.name
+}
+
 output "resource_group" {
     value = azurerm_resource_group.main.name
 }
